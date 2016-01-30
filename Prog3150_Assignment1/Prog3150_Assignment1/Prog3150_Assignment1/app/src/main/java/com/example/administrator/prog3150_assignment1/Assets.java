8dfe3b27697ada7a14c09b4cb2a36ab7cf96cccc
mdavison-cc <mdavison-cc@2A205-B03.conestogac.on.ca>
2016-01-29 20:32:36 -0500

    1/29/2016
    Added the fragments
    Started working on the Add fragment
    Added Preferences

---
+++ Prog3150_Assignment1/Prog3150_Assignment1/Prog3150_Assignment1/app/src/main/java/com/example/administrator/prog3150_assignment1/Assets.java
@@ -0,0 +1,14 @@
+package com.example.administrator.prog3150_assignment1;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by mdavison-cc on 1/29/2016.
+ */
+public class Assets {
+
+    public Assets()
+    {
+
+    }
+}
