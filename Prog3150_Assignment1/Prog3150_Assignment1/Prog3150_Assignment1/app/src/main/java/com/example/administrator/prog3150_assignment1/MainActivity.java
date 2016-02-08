9602ef30224667b1e33ecfc3cdec3aa86432d62f
BrandonErb <brandonerb@gmail.com>
2016-02-07 19:00:59 -0500

    Add some comments

--- Prog3150_Assignment1/Prog3150_Assignment1/Prog3150_Assignment1/app/src/main/java/com/example/administrator/prog3150_assignment1/MainActivity.java
+++ Prog3150_Assignment1/Prog3150_Assignment1/Prog3150_Assignment1/app/src/main/java/com/example/administrator/prog3150_assignment1/MainActivity.java
@@ -1,3 +1,10 @@
+/*
+MainActivity.java
+Assignment: Mobile Application Developement
+Brandon Davison, Brandon Erb and Nick Nathaniels
+Date: Feb 2, 2016
+Description: Mobile Application Development Assignment 1
+*/
 package com.example.administrator.prog3150_assignment1;
 
 import android.app.DatePickerDialog;
